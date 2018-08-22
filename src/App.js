1f9b1771fdbcf6a912d1d58aaa78ec707c11f6eb
tmt <thomas.culdaut@gmail.com>
2018-08-22 17:14:45 +0200

    passing state to props

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import "./App.css"
 import Movie from "./Movie"
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      selectedMovie: {
+        title: "Back to the Future",
+        plot:
+          "Marty McFly, a 17-year-old high school student, is accidentally sent thirty years into the past in a time-traveling DeLorean invented by his close friend, the maverick scientist Doc Brown.",
+        poster:
+          "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+      }
+    }
+  }
   render() {
     return (
       <Container>
@@ -14,11 +26,7 @@ class App extends Component {
         </Jumbotron>
         <Row>
           <Col xs="3">
-            <Movie
-              title="Back to the Future"
-              plot="Marty McFly, a 17-year-old high school student, is accidentally sent thirty years into the past in a time-traveling DeLorean invented by his close friend, the maverick scientist Doc Brown."
-              poster="https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
-            />
+            <Movie {...this.state.selectedMovie} />
           </Col>
         </Row>
       </Container>
