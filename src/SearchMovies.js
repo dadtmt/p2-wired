b6ff462dceaae8e9f34cf6057d8406ddd017b3ee
tmt <thomas.culdaut@gmail.com>
2018-08-22 17:34:33 +0200

    get input value from state

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -2,12 +2,18 @@ import React, { Component } from "react"
 import { Form, FormGroup, Label, Input } from "reactstrap"
 
 class SearchMovies extends Component {
+  constructor() {
+    super()
+    this.state = {
+      s: ""
+    }
+  }
   render() {
     return (
       <Form>
         <FormGroup>
           <Label for="searchMovies">Search movies</Label>
-          <Input />
+          <Input value={this.state.s} />
         </FormGroup>
       </Form>
     )
